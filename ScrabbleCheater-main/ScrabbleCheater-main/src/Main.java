6ce28b5ee7385af921c41678d380b5df851ecc81
joshu <jmao2500@gmail.com>
2025-07-18 20:16:06 +0200

    Added Method that allows to manually input words into the console, and then test them

--- ScrabbleCheater-main/ScrabbleCheater-main/src/Main.java
+++ ScrabbleCheater-main/ScrabbleCheater-main/src/Main.java
@@ -3,13 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         SimpleWordsList wordList = new SimpleWordsList();
-        wordList.initFromFile("ScrabbleCheater-main/SimpleWords.txt");
+        Scanner input = new Scanner(System.in);
+        wordList.initFromFile("ScrabbleCheater-main/ScrabbleCheater-main/SimpleWords.txt");
+
 
         //AUFGABE 2
-        System.out.println(wordList.getNormalized("hka"));
-        wordList.equals("bcda", "acdb");
+        //System.out.println(wordList.getNormalized("hka"));
+        //wordList.equals("bcda", "acdb");
 
         //AUFGABE 3
-        System.out.print("Ähnliche Wörter: " + wordList.validWordsUsingAllTiles("undh"));
+        //System.out.println("Ähnliche Wörter: " + wordList.validWordsUsingAllTiles("undh"));
+
+
+        // Aufgabe 6
+        testword(input, wordList);
+        testword(input, wordList);
+
+
+    }
+
+    public static void testword(Scanner input, SimpleWordsList wordList)
+    {
+        System.out.print("Input a Word to test: ");
+        String testInp = input.nextLine();
+
+        System.out.println("Ähnliche Wörter: " + wordList.validWordsUsingAllTiles(testInp));
     }
 }
